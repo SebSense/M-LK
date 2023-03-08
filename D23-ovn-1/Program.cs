dc72924689a1c8d7e3b3e62221adccdd9d4e79bc
SebSense <sebastian.senic@gmail.com>
2023-03-08 18:31:06 +0100

    Added tasks 42 - 48

--- D23-ovn-1/Program.cs
+++ D23-ovn-1/Program.cs
@@ -4,6 +4,8 @@ namespace D23_ovn_1
 {
     internal class Program
     {
+        //42. "Random" object
+        static Random rand = new Random();
         static void Main(string[] args)
         {
             //36. Loopa genom array
@@ -48,8 +50,109 @@ namespace D23_ovn_1
             Array.ForEach(nollTillNio, n => Console.Write(n + " "));
             Console.WriteLine("}");
             //42.
-
-
+            int ARRSIZE = 1000;
+            int[] rArr = new int[ARRSIZE];
+            for (int i = 0; i < ARRSIZE; i++)
+            {
+                rArr[i] = rand.Next(1, 7) + rand.Next(1, 7) + rand.Next(1, 7);
+            }
+            //Array.ForEach(rArr, n =>Console.Write(n + " "));
+            //43.
+            int[] stat = new int[19];
+            for (int i = 0; i < ARRSIZE; i++)
+            {
+                stat[rArr[i]]++;
+            }
+            for( int i = 0; i < stat.Length; i++)
+            {
+                Console.WriteLine(i + " " + stat[i]);
+            }
+            //44.
+            double[] femDubs = { 1.0, 2.5, 4, 3.0, 0.5 };
+            double[] tioDubs = new double[10];
+            double[] hundraDubs = new double[100];
+            FillRandomDoubles(tioDubs, rand);
+            FillRandomDoubles(hundraDubs, rand);
+            Console.Write("femDubs: ");
+            PrintArray(femDubs);
+            //44:
+            Console.WriteLine("44. " + SumDoubles(femDubs));
+            //45:
+            Console.WriteLine("45. " + AverageDoubles(femDubs));
+            //46:
+            Console.Write("46. Kopia : ");
+            PrintArray(CopyDoubles(femDubs));
+            //47.
+            Console.Write("47. Omvänd kopia : ");
+            PrintArray(CopyReverseDoubles(femDubs));
+            //48.
+            Console.Write("48. Insertion sort. Array: ");
+            PrintArray(tioDubs);
+            PrintArray(InsertionSortDoubles(tioDubs));
+            PrintArray(femDubs);
+            PrintArray(InsertionSortDoubles(femDubs));
+            PrintArray(femDubs);
         }
+        //44.
+        static double SumDoubles(double[] data)
+        {
+            double sum = 0;
+            foreach (double d in data) sum += d;
+            return sum;
+        }
+        //45.
+        static double AverageDoubles(double[] data)
+        {
+            double sum = SumDoubles(data);
+            return sum / data.Length;
+        }
+        //46.
+        static double[] CopyDoubles(double[] data)
+        {
+            double[] arr = new double[data.Length];
+            for( int i = 0; i < data.Length; i++ ) arr[i] = data[i];
+            return arr;
+        }
+        //47.
+        static double[] CopyReverseDoubles(double[] data)
+        {
+            int ln = data.Length, i = 0;
+            double[] arr = new double[ln];
+            while(i < ln)
+            {
+                arr[i] = data[ln - ++i];
+            }
+            return arr;
+        }
+        //48.
+        static double[] InsertionSortDoubles(double[] data)
+        {
+            int ln = data.Length;
+            double[] arr = CopyDoubles(data);
+            double temp;
+            for (int i = 0; i < ln; i++)
+            {
+                int j = i;
+                temp = arr[j];
+                while (j > 0 && temp < arr[j - 1]) arr[j] = arr[--j];
+                arr[j] = temp;
+            };
+            return arr;
+        }
+        static void PrintArray(double[] arr)
+        {
+            Console.Write("{ ");
+            Array.ForEach(arr, e => Console.Write(e + " "));
+            Console.WriteLine("}");
+        }
+        static void FillRandomDoubles(double[] data, Random r)
+        {
+            for (int i = 0, ln = data.Length; i < ln; i++)
+            {
+                data[i] = r.NextDouble() * r.Next(1, 10000);
+            }
+        }
+
+
     }
 }
