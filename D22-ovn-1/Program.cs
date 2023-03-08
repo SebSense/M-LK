3d81776f641d8dfffcb0948217ae604a9c3a1f31
SebSense <sebastian.senic@gmail.com>
2023-03-08 13:16:53 +0100

    D22-ovn-1: Added tasks 18-25

--- D22-ovn-1/Program.cs
+++ D22-ovn-1/Program.cs
@@ -65,7 +65,7 @@ namespace D22_ovn_1
             Console.WriteLine($"Det går {sekunderPerAr * 100m} sekunder på ett sekel.");
             //7. Ålder i år till sekunder:
             int alder = GetInt("Hur gammal är du? (antal år): ");
-            Console.WriteLine($"Du är {(ulong) (sekunderPerAr * alder)} sekunder gammal");
+            Console.WriteLine($"Du är {(ulong)(sekunderPerAr * alder)} sekunder gammal");
             //D22-ovn-1.2 - if-satser
             //8. Ålder
             alder = GetInt("Hur gammal är du? (antal år): ");
@@ -100,7 +100,7 @@ namespace D22_ovn_1
                 new String[] {"Grekland"},
                 new String[] {"Turkiet"},
                 new String[] {"Saudiarabien" } };
-            int[] kalenderÅr = { 1582, 1584, 1610, 1648, 1682, 1700, 1752, 1753, 1873, 1875, 1896, 1912, 1915, 1916, 1917, 1918, 1919, 1923, 1926, 2016};
+            int[] kalenderÅr = { 1582, 1584, 1610, 1648, 1682, 1700, 1752, 1753, 1873, 1875, 1896, 1912, 1915, 1916, 1917, 1918, 1919, 1923, 1926, 2016 };
 
             Console.WriteLine("\nKalendersystem - Visar vilket år ett land övergick från det Julianska till det Gregorianska kalendersystemet.");
             string input;
@@ -130,7 +130,7 @@ namespace D22_ovn_1
             //10-11. Växelström/vägguttag:
             //Gör denna med få element för att spara tid...
             Console.WriteLine("\n\nVäxelström/vägguttag. Anger elinformation om länder.");
-            while(true)
+            while (true)
             {
                 string uttagstyp;
                 int husvolttal, frekvens;
@@ -176,11 +176,48 @@ namespace D22_ovn_1
             }
 
             //D22-ovn-1.3 - static-metoder (funktioner)
-            //12-15:
+            //12-17:
             Console.WriteLine("Tria(1) = {0} Tria(2) = {1} Tria(3) = {2} Tria(4) = {3}", Tria(1), Tria(2), Tria(3), Tria(4));
             Console.WriteLine("Unitcirle för: 1 = {0}   2 = {1}   0 = {2}   0.5 = {3}   -2 = {4}", UnitCirle(1), UnitCirle(2), UnitCirle(0), UnitCirle(0.5), UnitCirle(-2));
             Console.WriteLine("Inverse(1.5) = {0} Inverse(0.75) = {1} Inverse(-2) = {2} Inverse(0) = {3}", Inverse(1.5), Inverse(0.75), Inverse(-2), Inverse(0));
             Console.WriteLine($"Max2(12, 10) = {Max2(12, 10)} Max2(3,-2) = {Max2(3, -2)} IsEven(9) = {IsEven(9)} IsEven(-8) = {IsEven(-8)}\nIsDivisible(56,8) = {IsDivisible(56, 8)} IsDivisible(91,9) = {IsDivisible(91, 9)}\n");
+
+            //D22-ovn-1.4 - loopar
+            //18. Räkna ut summan av alla tal mellan 7 och -5
+            int sum = 0;
+            for (int i = -5; i < 8; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine("summan av alla tal mellan 7 och -5 = " + sum);
+            //19:
+            ulong product = 1;
+            for (int i = 33; i < 45; i++)
+            {
+                product *= (ulong)i;
+            }
+            int test = 0;
+            Console.WriteLine("Produkten av alla tal mellan 33 och 44 (inklusivt) är " + product);
+            //20.
+            Console.WriteLine("\nProdukten av alla tal mellan två heltal");
+            Console.WriteLine(FactorialTwo(GetInt("Tal 1: "), GetInt("Tal 2: ")));
+            //21.
+            Console.WriteLine("\nProdukten av alla tal mellan två positiva heltal");
+            Console.WriteLine(FactorialPos(GetInt("Tal 1: "), GetInt("Tal 2: ")));
+            //22.
+            Console.WriteLine("\nProdukten av alla udda tal mellan två heltal:");
+            Console.WriteLine(FactorialOdd(GetInt("Tal 1: "), GetInt("Tal 2: ")));
+            //23.
+            Console.WriteLine("\nSumman av X + (X+1)...+Z där beräkningen avbryts när summan > 100.");
+            int[] sum100s = Sum100(GetInt("Ange tal: "));
+            Console.WriteLine($"Z = {sum100s[0]} Summa = {sum100s[1]}");
+            //24.
+            Console.WriteLine("\nSumman av X + (X+1)...+Z där beräkningen avbryts en iteration innan summan > 100.");
+            int[] sumBefore100s = SumBefore100(GetInt("Ange tal: "));
+            Console.WriteLine($"Z = {sumBefore100s[0]} Summa = {sumBefore100s[1]}");
+            //25.
+            Console.WriteLine("\nSumman av 1/m + 1/(m+1) .... + 1/(n-1) + 1/n ");
+            Console.WriteLine(SumOfInverse(GetInt("Ange m: "), GetInt("Ange n: ")));
 
         }
         static int Tria(int x) // 12. Tria.
@@ -199,13 +236,72 @@ namespace D22_ovn_1
         {
             return x > y ? x : y;
         }
-        static bool IsEven (int n) //16
+        static int Min2(int x, int y)
+        {
+            return x < y ? x : y;
+        }
+        static bool IsEven(int n) //16
         {
             return n % 2 == 0 ? true : false;
         }
-        static bool IsDivisible (int m, int n) //17
+        static bool IsDivisible(int m, int n) //17
         {
             return m % n == 0 ? true : false;
+        }
+        static long FactorialTwo(int x, int y) //21
+        {
+            // if (x < 1) x = 1;
+            // if (y < 1) y = 1;
+            long fact = 1,
+                hi = (long)Max2(x, y),
+                lo = x < y ? (long)x : (long)y;
+            while (lo <= hi) fact *= lo++;
+            return fact;
+        }
+        static ulong FactorialPos(int x, int y) //21
+        {
+            if (x < 1) x = 1;
+            if (y < 1) y = 1;
+            ulong fact = 1,
+                hi = (ulong)Max2(x, y),
+                lo = x < y ? (ulong)x : (ulong)y;
+            while (lo <= hi) fact *= lo++;
+            return fact;
+        }
+        static long FactorialOdd(int x, int y) //22
+        {
+            // if (x < 1) x = 1;
+            // if (y < 1) y = 1;
+            long fact = 1,
+                hi = (long)Max2(x, y),
+                lo = x < y ? (long)x : (long)y;
+            while (lo <= hi) fact *= IsEven((int)lo++) ? 1 : lo - 1;
+            return fact;
+        }
+        static int[] Sum100(int x) //23
+        {
+            int sum = 0;
+            do
+            {
+                sum += x++;
+            } while (sum < 100);
+            return new int[] { x - 1, sum };
+        }
+        static int[] SumBefore100(int x) //24
+        {
+            int sum = 0;
+            do
+            {
+                sum += x;
+            } while (sum + x++ < 100);
+            return new int[] { x - 1, sum};
+        }
+        static double SumOfInverse(int m, int n) //25
+        {
+            double sum = 0;
+            int lo = Min2(m, n), hi = Max2(m, n);
+            while (lo <= hi) sum += Inverse(lo++);
+            return sum;
         }
     }
 }
