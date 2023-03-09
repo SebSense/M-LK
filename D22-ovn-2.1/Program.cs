d1b2b32c2e40a17762fba6e51581842684d80664
SebSense <sebastian.senic@gmail.com>
2023-03-09 22:27:49 +0100

    INITIAL COMMIT for D22-ovn-2.1

---
+++ D22-ovn-2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D22_ovn_2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
