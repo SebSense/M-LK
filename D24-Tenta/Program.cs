7dbaad50e8c8fc620e3e50c1c63fe5a9324bebc0
SebSense <sebastian.senic@gmail.com>
2023-03-10 09:18:18 +0100

    Löst uppgift 2

--- D24-Tenta/Program.cs
+++ D24-Tenta/Program.cs
@@ -9,9 +9,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("D24-Tenta.exe - Numrerade uppgifter från tentamen\n");
+            
             //Uppgift 1: Variabel och utskrift:
             double minutesForSunlightToReachEarth = (SUN_DISTANCE_IN_KM / C_IN_KMPS) / SEC_PER_MIN;
             Console.WriteLine("Uppgift 1: Det tar " + minutesForSunlightToReachEarth + " minuter för solens ljus att nå jorden\n");
+
+            //Uppgift 2: Test och utskrifter:
+            Console.WriteLine($"Uppgift 2: Test av 'InvSqr(double x)' static-metod:\n" +
+                $" InvSqr(2) = {InvSqr(2)}\n" +
+                $" InvSqr(3) = {InvSqr(3)}\n" +
+                $" InvSqr(4) = {InvSqr(4)}\n" +
+                $" InvSqr(5) = {InvSqr(5)}\n");
+
+        }
+        //Uppgift 2: Static-metod:
+        static double InvSqr(double x)
+        {
+            return 1 / (x * x);
         }
     }
 }
