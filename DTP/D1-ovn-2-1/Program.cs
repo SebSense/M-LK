32c855c3682c305d758d280a80f10ae73d22b831
SebSense <sebastian.senic@gmail.com>
2023-03-13 15:41:48 +0100

    Initial commit

---
+++ DTP/D1-ovn-2-1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace D1_ovn_2_1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Räknar antal ord i en fil
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string input = "count", path = ".", filename = "testfile.txt";
+            do
+            {
+                //Fråga efter kommdo
+                if(input == "count")
+                {
+                    //Fråga efter filnamn
+                    //Anropa metoder för att räkna:
+                    //Antal rader
+                    //Antal tecken
+                    //Antal ord
+                    //Printa ut alla tre rad för rad
+                }
+                //LÄGST PRIO:
+                //Kommandon för att navigera filstruktur:
+                //else if (dir)
+                //else if (cd)
+            } while(input != "quit");
+            
+        }
+    }
+}
