f8fbeaee2e855f4c36f690fa3e183666852c14e9
SebSense <sebastian.senic@gmail.com>
2023-03-13 16:46:22 +0100

    Added character and word counter

--- DTP/D1-ovn-2-1/Program.cs
+++ DTP/D1-ovn-2-1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace D1_ovn_2_1
 {
@@ -20,7 +21,9 @@ namespace D1_ovn_2_1
                     string text = File.ReadAllText(@"D:\dev\moelk\repos\DTP\D1-ovn-2-1\obj\Debug\net6.0\testfile.txt");
                     Console.WriteLine(text);
                     Console.WriteLine($"Antal rader i {filename}: " + (1 + text.Count(c => c == '\n')));
-                    
+                    Console.WriteLine($"Antal tecken i {filename}: " + text.Count(c => !Array.Exists("\n\t\0 ".ToCharArray(), elem => elem == c)));
+                    Regex regWord = new Regex(@"(^[A-Za-z0-9])|(?<=[ \n\t])[A-Za-z0-9]");
+                    Console.WriteLine($"Antal ord i {filename}: " + regWord.Matches(text).Count);
                     input = "quit";
                     //Anropa metoder för att räkna:
                     //Antal rader
