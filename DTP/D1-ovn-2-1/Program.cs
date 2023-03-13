e539ae4185b387a85e666ce4b17f42049de8ee10
SebSense <sebastian.senic@gmail.com>
2023-03-13 17:01:46 +0100

    Added full functionality according to spec

--- DTP/D1-ovn-2-1/Program.cs
+++ DTP/D1-ovn-2-1/Program.cs
@@ -11,25 +11,25 @@ namespace D1_ovn_2_1
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string input = "count", path = Directory.GetCurrentDirectory(), filename = "testfile.txt";
+            string input, path = Directory.GetCurrentDirectory(), filename;
+            Console.WriteLine("Text counter: Welcome to the text counter program.\nThis program counts lines, characters and words in .txt-files.");
             do
             {
-                //Fråga efter kommdo
-                if(input == "count")
+                Console.WriteLine("Commands are 'count' and 'quit'");
+                Console.Write("Command: ");
+                input = Console.ReadLine();
+                if (input == "count")
                 {
-                    //Ändra till att fråga efter filnamn
-                    string text = File.ReadAllText(@"D:\dev\moelk\repos\DTP\D1-ovn-2-1\obj\Debug\net6.0\testfile.txt");
-                    Console.WriteLine(text);
+                    Console.Write("Input filenamne: ");
+                    filename = Console.ReadLine();
+                    string fullPath = path + "\\" + filename;
+                    if (!File.Exists(fullPath)) { Console.WriteLine("Cannot find file"); continue; }
+                    string text = File.ReadAllText(fullPath);
+                    //Console.WriteLine(text);
                     Console.WriteLine($"Antal rader i {filename}: " + (1 + text.Count(c => c == '\n')));
                     Console.WriteLine($"Antal tecken i {filename}: " + text.Count(c => !Array.Exists("\n\t\0 ".ToCharArray(), elem => elem == c)));
                     Regex regWord = new Regex(@"(^[A-Za-z0-9])|(?<=[ \n\t])[A-Za-z0-9]");
                     Console.WriteLine($"Antal ord i {filename}: " + regWord.Matches(text).Count);
-                    input = "quit";
-                    //Anropa metoder för att räkna:
-                    //Antal rader
-                    //Antal tecken
-                    //Antal ord
-                    //Printa ut alla tre rad för rad
                 }
                 //LÄGST PRIO:
                 //Kommandon för att navigera filstruktur:
