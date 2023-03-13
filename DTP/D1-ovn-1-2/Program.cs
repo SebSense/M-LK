6f08e9366956c2c84b3c7e11f68d6eeb5aaa76d7
SebSense <sebastian.senic@gmail.com>
2023-03-13 11:45:56 +0100

    Task finished

--- DTP/D1-ovn-1-2/Program.cs
+++ DTP/D1-ovn-1-2/Program.cs
@@ -26,18 +26,55 @@ namespace D1_ovn_1_2
                 språk.ForEach(språk => nyttOrd.Add(GetString("Ange ordet på " + språk + " :")));
                 ord.Add(nyttOrd);
             }
+            public void AddWordHardCode(string[] strings)
+            {
+                ord.Add(strings.ToList());
+            }
             public void Visa()
             {
-                Console.Write("\n     ");
+                Console.Write("\n    ");
                 språk.ForEach((string s) => Console.Write(s + "\t\t"));
                 Console.WriteLine("\n---------------------------------------------------------------------");
-                int i = 1;
                 foreach(List<string> word in ord)
                 {
-                    Console.Write((i < 10 ? "  " + i++ + ". " : (i < 100 ? " " + i++ + ". " : i++ + ". ")));
-                    word.ForEach((string w) => Console.Write(w + (w.Length < 3 ? "\t\t\t" : (w.Length > 10 ? "\t" : "\t\t"))));
+                    Console.Write("    ");
+                    word.ForEach((string w) => Console.Write(w + (w.Length < 4 ? "\t\t\t" : (w.Length > 10 ? "\t" : "\t\t"))));
                     Console.WriteLine();
                 }
+            }
+            public bool HasLanguage(string language)
+            {
+                return språk.Contains(language);
+            }
+            public void FindWord(string search, string language)
+            {
+                int index = språk.IndexOf(language);
+                foreach(List<string> word in ord)
+                {
+                    if(word[index] == search)
+                    {
+                        Console.Write("\n");
+                        språk.ForEach((string s) => Console.Write(s + ":\t\t"));
+                        Console.WriteLine();
+                        word.ForEach((string w) => Console.Write(w + (w.Length < 8 ? "\t\t" : (w.Length > 10 ? "\t" : "\t"))));
+                        Console.WriteLine();
+                        return;
+                    }
+                }
+                Console.WriteLine("\nOrdet " + search + " på " + language + " finns inte i ordlistan.");
+            }
+            public void DeleteWord(string search)
+            {
+                foreach (List<string> word in ord)
+                {
+                    if (word.Contains(search))
+                    {
+                        ord.Remove(word);
+                        Console.WriteLine(search + " borttaget ur orlistan.");
+                        return;
+                    }   
+                }
+                Console.WriteLine("\nOrdet " + search + " finns inte i ordlistan.");
             }
         }
         public class Ord
@@ -51,7 +88,8 @@ namespace D1_ovn_1_2
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Ordbok ordbok = new(new string[]{ "svenska", "latin" });
+            Ordbok ordbok = new(new string[]{ "latin", "svenska" });
+            ordbok.AddWordHardCode(new string[] { "annum", "år" });
             Welcome();
             string input;
             string[] words;
@@ -64,10 +102,6 @@ namespace D1_ovn_1_2
                 {
                     Help();
                 }
-                else if (input == "latin")
-                {
-                    NotImplemented();
-                }
                 else if (input == "ny")
                 {
                     ordbok.AddWord();
@@ -77,17 +111,19 @@ namespace D1_ovn_1_2
                     Console.WriteLine("Adjö!");
                     return;
                 }
-                else if (input == "svenska")
-                {
-                    NotImplemented();
-                }
                 else if (input == "ta bort")
                 {
-                    NotImplemented();
+                    ordbok.DeleteWord(GetString("Ta bort vilket ord? :"));
                 }
                 else if (input == "visa")
                 {
                     ordbok.Visa();
+                }
+                else if (ordbok.HasLanguage(input))
+                {
+                    Console.Write("Slår upp ord på " + input + ". Ange ord: ");
+                    ordbok.FindWord(Console.ReadLine(), input);
+
                 }
             }
         }
@@ -96,7 +132,7 @@ namespace D1_ovn_1_2
         /// </summary>
         static void Welcome()
         {
-            NotImplemented();
+            Console.WriteLine("Välkommen till den latinsk-svenska ordboken. Skriv 'hjälp' för att lista kommandon.\n");
         }
         /// <summary>
         /// Placeholder method
@@ -110,7 +146,14 @@ namespace D1_ovn_1_2
         /// </summary>
         static void Help()
         {
-            NotImplemented();
+            Console.WriteLine("\nLatinsk-svenska ordboken. Detta är en ordbok mellan latin och svenska. Använd följande kommandon:\n" +
+                "  visa        - visar hela ordlistan\n" +
+                "  ny          - lägg till ett nytt ord till ordlistan\n" +
+                "  ta bort     - ta bort ett ord ur ordlistan\n" +
+                "  latin       - sök efter ett ord på latin och visa översättning\n" +
+                "  svenska     - sök efter ett ord på svenska och visa översättning\n" +
+                "  hjälp       - visa denna hjälp\n" +
+                "  sluta       - avsluta programmet\n");
         }
         static string GetString(string prompt)
         {
