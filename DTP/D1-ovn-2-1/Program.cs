043ad84d22bc9191f0ff3a0d50f6d808a631bc77
SebSense <sebastian.senic@gmail.com>
2023-03-13 17:09:14 +0100

    Added D1-ovn-2-2 stat command

--- DTP/D1-ovn-2-1/Program.cs
+++ DTP/D1-ovn-2-1/Program.cs
@@ -15,7 +15,7 @@ namespace D1_ovn_2_1
             Console.WriteLine("Text counter: Welcome to the text counter program.\nThis program counts lines, characters and words in .txt-files.");
             do
             {
-                Console.WriteLine("Commands are 'count' and 'quit'");
+                Console.WriteLine("Commands are 'count' and 'quit' and 'stat'");
                 Console.Write("Command: ");
                 input = Console.ReadLine();
                 if (input == "count")
@@ -30,6 +30,16 @@ namespace D1_ovn_2_1
                     Console.WriteLine($"Antal tecken i {filename}: " + text.Count(c => !Array.Exists("\n\t\0 ".ToCharArray(), elem => elem == c)));
                     Regex regWord = new Regex(@"(^[A-Za-z0-9])|(?<=[ \n\t])[A-Za-z0-9]");
                     Console.WriteLine($"Antal ord i {filename}: " + regWord.Matches(text).Count);
+                }
+                else if (input == "stat")
+                {
+                    Console.Write("Input filenamne: ");
+                    filename = Console.ReadLine();
+                    string fullPath = path + "\\" + filename;
+                    if (!File.Exists(fullPath)) { Console.WriteLine("Cannot find file"); continue; }
+                    string text = File.ReadAllText(fullPath).ToLower();
+                    string chars = "abcdefghijklmnopqrstuvwxyz";
+                    foreach(char c in chars) Console.WriteLine(c + ": " + text.Count(e => e == c));
                 }
                 //LÄGST PRIO:
                 //Kommandon för att navigera filstruktur:
