6a9a40fa94fb7cb92f8efb39d958a297c37afb3d
SebSense <sebastian.senic@gmail.com>
2023-03-13 15:53:45 +0100

    Added loading file

--- DTP/D1-ovn-2-1/Program.cs
+++ DTP/D1-ovn-2-1/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace D1_ovn_2_1
+﻿using System.Diagnostics;
+
+namespace D1_ovn_2_1
 {
     internal class Program
     {
@@ -8,13 +10,16 @@
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            string input = "count", path = ".", filename = "testfile.txt";
+            string input = "count", path = Directory.GetCurrentDirectory(), filename = "testfile.txt";
             do
             {
                 //Fråga efter kommdo
                 if(input == "count")
                 {
                     //Fråga efter filnamn
+                    string text = File.ReadAllText(@"D:\dev\moelk\repos\DTP\D1-ovn-2-1\obj\Debug\net6.0\testfile.txt");
+                    Console.Write(text);
+                    input = "quit";
                     //Anropa metoder för att räkna:
                     //Antal rader
                     //Antal tecken
