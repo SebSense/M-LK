77495e71039f95f4cb29735d831c61915b52d9db
SebSense <sebastian.senic@gmail.com>
2023-03-14 11:36:57 +0100

    Class, open, save, edit, quit added

--- DTP/D1-ovn-3/Program.cs
+++ DTP/D1-ovn-3/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using System.IO.Enumeration;
+using System.Runtime.CompilerServices;
 
 namespace D1_ovn_3
 {
@@ -22,29 +25,68 @@ namespace D1_ovn_3
                 this.title = lines[2];
                 this.link = lines[3];
             }
+            public void Print()
+            {
+                Console.WriteLine(" Topic: " + topic + "\n   Site: " + site + "" +
+                    "\n   Title: " + title + "\n   Link: " + link);
+            }
         }
         static void Main(string[] args)
         {
             //NYI: En process för att öppna och ändra i filer.
-            Process notepad = new Process();
-            notepad.StartInfo.FileName = "notepad";
+
             
-            //Öppna fil...
-            string[] text_lines = File.ReadAllLines("weblinks.lis");
             List<Link> weblinks = new();
-            Array.ForEach(text_lines, line => weblinks.Add(new Link(line)));
-            //Testprint
-            foreach(Link link in weblinks)
-            {
-                Console.WriteLine("Topic: " + link.topic + "\nSite: " + link.site + "" +
-                    "\nTitle: " + link.title + "\nLink: " + link.link);
-            }
-            //TBD: Main while-loop
+            //Öppna fil...
+            string path = Directory.GetCurrentDirectory();
+            string filename = null;
+            string input;
             do
             {
-                //nested if, else... commands:
-                //
-            }while(true);
+                Console.Write(":");
+                input = Console.ReadLine();
+                string[] words = input.Split(' ');
+                int ln = words.Length;
+                if(ln == 2 && words[0] == "edit")
+                {
+                    filename = words[1];
+                    Process.Start("notepad", filename);
+                }
+                else if (ln == 2 && words[0] == "load")
+                {
+                    filename = words[1];
+                    string[] filetext_lines = File.ReadAllLines(path + "\\" + filename);
+                    Array.ForEach(filetext_lines, line => weblinks.Add(new Link(line)));
+                }
+                else if (input == "list")
+                {
+                    for(int i = 0; i < weblinks.Count; i++)
+                    {
+                        Console.Write((i + 1) + ".");
+                        weblinks[i].Print();
+                    }
+                }
+                else if (input == "open")
+                {
+
+                }
+                else if (input == "save")
+                {
+                    StreamWriter writer = new StreamWriter(path + "\\" + filename);
+                    for(int i = 0; i < weblinks.Count; i++)
+                    {
+                        Link link = weblinks[i];
+                        if(i != 0) writer.WriteLine();
+                        writer.Write(String.Join(';', link.topic, link.site, link.title, link.link));
+                    }
+                    writer.Close();
+                }
+                else if (input == "quit")
+                {
+                    Console.WriteLine("Adjö!");
+                    return;
+                }
+            } while(true);
         }
     }
 }
